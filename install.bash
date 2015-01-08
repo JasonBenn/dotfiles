cfa1b0b518fb81947980e315ca4f7b4ea22b1dca
Jason Benn <jasoncbenn@gmail.com>
2015-01-08 14:06:29 -0800

    Symlinking script.

---
+++ install.bash
@@ -0,0 +1,11 @@
+# use work_bash_profile
+ln -sf /Users/jasoncbenn/code/dotfiles/work_bash_profile.bash /Users/jasoncbenn/.bash_profile
+
+# use home_bash_profile
+# ln -sf /Users/jasoncbenn/code/dotfiles/home_bash_profile.bash /Users/jasoncbenn/.bash_profile
+
+# install sublime settings
+ln -sf /Users/jasoncbenn/code/dotfiles/sublime_settings /Users/jasoncbenn/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+# install work ssh_config
+ln -sf /Users/jasoncbenn/code/dotfiles/ssh_config /Users/jasoncbenn/.ssh/config
