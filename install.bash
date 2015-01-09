e3fbb008a58599ae293538313eba9125ac98cecb
Jason Benn <jasoncbenn@gmail.com>
2015-01-08 22:18:15 -0800

    update sublime settings

--- install.bash
+++ install.bash
@@ -1,8 +1,8 @@
 # use work_bash_profile
-ln -sf /Users/jasoncbenn/code/dotfiles/work_bash_profile.bash /Users/jasoncbenn/.bash_profile
+# ln -sf /Users/jasoncbenn/code/dotfiles/work_bash_profile.bash /Users/jasoncbenn/.bash_profile
 
 # use home_bash_profile
-# ln -sf /Users/jasoncbenn/code/dotfiles/home_bash_profile.bash /Users/jasoncbenn/.bash_profile
+ln -sf /Users/jasoncbenn/code/dotfiles/home_bash_profile.bash /Users/jasoncbenn/.bash_profile
 
 # install sublime settings
 ln -sf /Users/jasoncbenn/code/dotfiles/sublime_settings /Users/jasoncbenn/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
