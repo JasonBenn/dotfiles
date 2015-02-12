6b3e8f411e3f48ba62608253028ef30a45121fc8
Jason Benn <jasoncbenn@gmail.com>
2015-02-12 14:49:55 -0800

    Remove RVM.

--- work_bash_profile.bash
+++ work_bash_profile.bash
@@ -88,7 +88,6 @@ function gacp {
 
 source /Users/jasoncbenn/code/dotfiles/git-completion.bash
 source ~/.profile
-source ~/.rvm/scripts/rvm
 # For `bundle open gemname`
 EDITOR='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 
