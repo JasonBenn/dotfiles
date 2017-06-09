17a1308556d9bab7ca1eec811fbeca804682a170
Jason Benn <jasoncbenn@gmail.com>
2017-06-08 17:00:07 -0700

    Updated xlarge status script again.

--- check-p2-xlarge-status.sh
+++ check-p2-xlarge-status.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 while true; do
-  STATE="$(aws ec2 describe-instances --filters "Name=instance-type,Values=p2.xlarge" | grep STATE | grep -v STATEREASON  | awk '{ print $3 }')"
-  osascript -e 'display notification "'${STATE}'" with title "p2.xlarge status"'
-  sleep 600
+  STATE="$(aws ec2 describe-instances --filters Name=instance-type,Values=p2.xlarge --query Reservations[0].Instances[0].State.Name)"
+  if [ "$STATE" = '"running"' ]; then
+    COMMAND='display notification '$STATE' with title "p2.xlarge status"'
+    osascript -e "$COMMAND"
+    sleep 300
+  fi
 done
